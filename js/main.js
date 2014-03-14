7e8b905cc5d76fdd7b0f8fe6c55b311b6db35a15
Raymi T. Eldby <raymitoro@gmail.com>
2014-03-14 01:29:39 +0100

    Testfix #15

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ jQuery(document).ready(function($) {
 
     banner_toggler.bind('click', toggle_banner_on);     
 
-    if(!Modernizr.cssvhunit) { $("#top-content").height(window.innerHeight) + Math.abs(window.orientation) == 90 ? 42 :; }
+    if(!Modernizr.cssvhunit) { $("#top-content").height(window.innerHeight) + Math.abs(window.orientation) == 90 ? 42 : 0; }
 
     var onOrientationChange = function() {
     	$("#top-content").height(window.innerHeight);
